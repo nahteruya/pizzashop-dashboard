88386bf0d81ff0d544a3446f43ffab28864eab0b
Naomi Teruya <nahteruya@gmail.com>
2024-07-17 10:09:16 -0300

    chore: setup react-helmet-async for managing pages title

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Helmet, HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router-dom";
 
 import { routes } from "./routes";
 
 export function App() {
-  return <RouterProvider router={routes} />;
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <RouterProvider router={routes} />
+    </HelmetProvider>
+  );
 }
