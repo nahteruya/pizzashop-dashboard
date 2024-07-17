1477a87b843bff7243f24241df451dfef46679da
Naomi Teruya <nahteruya@gmail.com>
2024-07-17 09:33:33 -0300

    feat: add react-router-dom for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { Button } from "./components/ui/button";
+import { RouterProvider } from "react-router-dom";
+
+import { routes } from "./routes";
 
 export function App() {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <Button>Enviar</Button>
-    </div>
-  );
+  return <RouterProvider router={routes} />;
 }
