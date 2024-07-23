81eb3153666c7ead27a9ce9bc429b6f31bff9d7c
Naomi Teruya <nahteruya@gmail.com>
2024-07-23 06:33:34 -0300

    feat: add pagination feature

--- src/api/get-orders.ts
+++ src/api/get-orders.ts
@@ -1,5 +1,9 @@
 import { api } from "@/lib/axios";
 
+interface GetOrdersQuery {
+  pageIndex: number | null;
+}
+
 export interface getOrdersResponse {
   orders: {
     orderId: string;
@@ -15,9 +19,9 @@ export interface getOrdersResponse {
   };
 }
 
-export async function getOrders() {
+export async function getOrders({ pageIndex }: GetOrdersQuery) {
   const response = await api.get<getOrdersResponse>("/orders", {
-    params: { pageIndex: 0 },
+    params: { pageIndex },
   });
   return response.data;
 }
--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,5 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { Helmet } from "react-helmet-async";
+import { useSearchParams } from "react-router-dom";
+import { z } from "zod";
 
 import { getOrders } from "@/api/get-orders";
 import {
@@ -15,10 +17,25 @@ import { OrderTableRow } from "./order-table-row";
 import { Pagination } from "./pagination";
 
 export function Orders() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const pageIndex = z.coerce
+    .number()
+    .transform((page) => page - 1)
+    .parse(searchParams.get("page") ?? "1");
+
   const { data: result } = useQuery({
-    queryKey: ["orders"],
-    queryFn: getOrders,
+    queryKey: ["orders", pageIndex],
+    queryFn: () => getOrders({ pageIndex }),
   });
+
+  function handlePaginate(pageIndex: number) {
+    setSearchParams((prev) => {
+      prev.set("page", (pageIndex + 1).toString());
+      return prev;
+    });
+  }
+
   return (
     <>
       <Helmet title="Pedidos" />
@@ -51,7 +68,14 @@ export function Orders() {
           </div>
         </div>
 
-        <Pagination pageIndex={0} perPage={10} totalCount={105} />
+        {result && (
+          <Pagination
+            pageIndex={result.meta.pageIndex}
+            perPage={result.meta.perPage}
+            totalCount={result.meta.totalCount}
+            onPageChange={handlePaginate}
+          />
+        )}
       </div>
     </>
   );
