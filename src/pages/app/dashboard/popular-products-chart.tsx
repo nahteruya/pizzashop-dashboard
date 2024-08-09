86cca38e14e60744680f268f816ce2fb00c08df0
Naomi Teruya <nahteruya@gmail.com>
2024-08-09 07:55:33 -0300

    feat: add loading to onloading charts data

--- src/pages/app/dashboard/popular-products-chart.tsx
+++ src/pages/app/dashboard/popular-products-chart.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { BarChart } from "lucide-react";
+import { BarChart, Loader2 } from "lucide-react";
 import { Cell, Pie, PieChart, ResponsiveContainer } from "recharts";
 import colors from "tailwindcss/colors";
 
@@ -34,60 +34,67 @@ export function PopularProductsChart() {
         </div>
       </CardHeader>
       <CardContent>
-        <ResponsiveContainer width="100%" height={248}>
-          <PieChart style={{ fontSize: 12 }}>
-            {data && (
-              <Pie
-                data={data}
-                dataKey="amount"
-                nameKey="product"
-                cx="50%"
-                cy="50%"
-                outerRadius={86}
-                innerRadius={64}
-                strokeWidth={8}
-                labelLine={false}
-                label={({
-                  cx,
-                  cy,
-                  midAngle,
-                  innerRadius,
-                  outerRadius,
-                  value,
-                  index,
-                }) => {
-                  const RADIAN = Math.PI / 180;
-                  const radius = 12 + innerRadius + (outerRadius - innerRadius);
-                  const x = cx + radius * Math.cos(-midAngle * RADIAN);
-                  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+        {getPopularProductsFn ? (
+          <ResponsiveContainer width="100%" height={248}>
+            <PieChart style={{ fontSize: 12 }}>
+              {data && (
+                <Pie
+                  data={data}
+                  dataKey="amount"
+                  nameKey="product"
+                  cx="50%"
+                  cy="50%"
+                  outerRadius={86}
+                  innerRadius={64}
+                  strokeWidth={8}
+                  labelLine={false}
+                  label={({
+                    cx,
+                    cy,
+                    midAngle,
+                    innerRadius,
+                    outerRadius,
+                    value,
+                    index,
+                  }) => {
+                    const RADIAN = Math.PI / 180;
+                    const radius =
+                      12 + innerRadius + (outerRadius - innerRadius);
+                    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+                    const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
-                  return (
-                    <text
-                      x={x}
-                      y={y}
-                      className="fill-muted-foreground text-xs"
-                      textAnchor={x > cx ? "start" : "end"}
-                      dominantBaseline="central"
-                    >
-                      {data[index].product.length > 12
-                        ? data[index].product.substring(0, 12).concat("...")
-                        : data[index].product}{" "}
-                      ({value})
-                    </text>
-                  );
-                }}
-              >
-                {data.map((_, i) => (
-                  <Cell
-                    key={`cell-${i}`}
-                    fill={COLORS[i]}
-                    className="stroke-background"
-                  />
-                ))}
-              </Pie>
-            )}
-          </PieChart>
-        </ResponsiveContainer>
+                    return (
+                      <text
+                        x={x}
+                        y={y}
+                        className="fill-muted-foreground text-xs"
+                        textAnchor={x > cx ? "start" : "end"}
+                        dominantBaseline="central"
+                      >
+                        {data[index].product.length > 12
+                          ? data[index].product.substring(0, 12).concat("...")
+                          : data[index].product}{" "}
+                        ({value})
+                      </text>
+                    );
+                  }}
+                >
+                  {data.map((_, i) => (
+                    <Cell
+                      key={`cell-${i}`}
+                      fill={COLORS[i]}
+                      className="stroke-background"
+                    />
+                  ))}
+                </Pie>
+              )}
+            </PieChart>
+          </ResponsiveContainer>
+        ) : (
+          <div className="flex h-[240px] w-full items-center justify-center">
+            <Loader2 className="h-8 w-8 animate-spin text-muted-foreground" />
+          </div>
+        )}
       </CardContent>
     </Card>
   );
--- src/pages/app/dashboard/revenue-chart.tsx
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { subDays } from "date-fns";
+import { Loader2 } from "lucide-react";
 import { useMemo, useState } from "react";
 import { DateRange } from "react-day-picker";
 import {
@@ -11,7 +12,6 @@ import {
   YAxis,
 } from "recharts";
 import colors from "tailwindcss/colors";
-import { date } from "zod";
 
 import { getDailyRevenueInPeriod } from "@/api/get-daily-revenue-in-period";
 import {
@@ -63,7 +63,7 @@ export function RevenueChart() {
         </div>
       </CardHeader>
       <CardContent>
-        {chartDate && (
+        {chartDate ? (
           <ResponsiveContainer width="100%" height={248}>
             <LineChart data={chartDate} style={{ fontSize: 12 }}>
               <XAxis dataKey="date" axisLine={false} tickLine={false} dy={16} />
@@ -91,6 +91,10 @@ export function RevenueChart() {
               />
             </LineChart>
           </ResponsiveContainer>
+        ) : (
+          <div className="flex h-[240px] w-full items-center justify-center">
+            <Loader2 className="h-8 w-8 animate-spin text-muted-foreground" />
+          </div>
         )}
       </CardContent>
     </Card>
